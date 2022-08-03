de34bb8d32a3ab7a1b2a162e2d1133c1d1b393f7
JonatanPilichowski <108926973+JonatanPilichowski@users.noreply.github.com>
2022-08-03 22:18:15 +0200

    Program GuessTheNumber working, core not refactored.

--- src/main/java/GuessTheNumber.java
+++ src/main/java/GuessTheNumber.java
@@ -2,61 +2,66 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
-   static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         int numberToGuess = randomNumber();
-        System.out.println("Guess the number (from 0 to 99)! You have 5 chances. " + numberToGuess);
+        System.out.println("Guess the number (from 0 to 99)! You have 5 chances.");
 
-        /*
-        int number = getNumber(inputFromUser());
-        System.out.println("Twoja liczba to: "+ number);
-        System.exit(0);
-
-         */
 
         for (int i = 0; i < 5; i++) {
-            String inputFromUser=inputFromUser();
-            int inputNumber = InsertEndlesslyUntilValueIsInt(inputFromUser);
+            String inputValue = inputFromUser();
+            inputValue = repeatInputValueUntilInRange(inputValue);
+            int inputNumber = getNumber(inputValue);
 
-            while(isValueNotInRange(inputNumber)){
-                inputNumber= InsertEndlesslyUntilValueIsInt(inputFromUser);
-
-                System.out.println("You've inserted number: "+ inputNumber + ", which is out of range. Expected range is 0-99");
-            }
-
-                if(inputNumber==numberToGuess){
-                    System.out.println("You guessed it!");
-                    break;
+            if (inputNumber == numberToGuess) {
+                System.out.println("You guessed it!");
+                break;
+            } else {
+                if ((4 - i) > 0) {
+                    guideIfUserInputHigherOrLowerThanRandNumber(numberToGuess, inputNumber);
+                    System.out.println("Please try again. You have: " + (4 - i) + " chance(s) left!");
                 } else {
-                    if (inputNumber>numberToGuess){
-                        System.out.println("Your number is GREATER than the one you are trying to guess.");
-                    } else {
-
-                        System.out.println("Your number is LOWER than the one you are trying to guess.");
-                    }
-                    System.out.println("Please try again. You have: "+ (4-i)+ " chances left!");
+                    System.out.println("Sorry you didn't guess the number, the answer was: " + numberToGuess);
                 }
-
             }
+        }
+    }
 
+    private static void guideIfUserInputHigherOrLowerThanRandNumber(int numberToGuess, int inputNumber) {
+        if (inputNumber > numberToGuess) {
+            System.out.println("Your number is GREATER than the one you are trying to guess.");
+        } else {
 
-        System.out.println("");
-        System.out.println("The end");
+            System.out.println("Your number is LOWER than the one you are trying to guess.");
+        }
+    }
 
+    private static String repeatInputValueUntilInRange(String inputValue) {
+        while (!isInputValidated(inputValue)) {
+            inputValue = inputFromUser();
+        }
+        return inputValue;
     }
 
     private static int randomNumber() {
         return new Random().nextInt(100);
     }
 
-    private static int InsertEndlesslyUntilValueIsInt(String inputFromUser) {
-        while (!isValueInt(inputFromUser)) {
-            System.out.println("Only numbers are accepted. This entry won't be counted as a guess. Please try again!");
-            inputFromUser=inputFromUser();
+    private static boolean isInputValidated(String inputFromUser) {
+        if (isValueInt(inputFromUser)) {
+            if (!isValueNotInRange(getNumber(inputFromUser))) {
+                return true;
+            } else {
+                System.out.println("Value is not in range. This entry won't be counted as a guess. You can pick only numbers from 0 to 99 range.");
+                return false;
+            }
+
+        } else {
+            System.out.println("Only whole numbers are accepted. This entry won't be counted as a guess. You can pick only numbers from 0-99 range.");
+            return false;
         }
-        return  getNumber(inputFromUser);
     }
 
     private static boolean isValueNotInRange(int numberFromUser) {
@@ -67,13 +72,13 @@ public class GuessTheNumber {
         try {
             getNumber(inputString);
             return true;
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             return false;
         }
     }
 
     private static Integer getNumber(String inputString) {
-       return Integer.parseInt(inputString);
+        return Integer.parseInt(inputString);
     }
 
     private static String inputFromUser() {
